30935692f5219cc6a821c8b10cb097321453f179
anthowen <webtal.blizzard@gmail.com>
2020-06-23 06:18:09 -0400

    Add baseui, styletron

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { LightTheme, BaseProvider } from "baseui";
+
+const engine = new Styletron();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <App />
+      </BaseProvider>
+    </StyletronProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
